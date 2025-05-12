11d190872ab4bbff900faec7fda3ffd2cf29d8d7
Megha-TK <meghakarumbayat@gmail.com>
2025-05-12 10:46:01 +0530

    Create sam.java

---
+++ sam.java
@@ -0,0 +1,11 @@
+import java.util.Scanner; // Import the Scanner class
+
+class sam {
+  public static void main(String[] args) {
+    int x=10;
+     int y=20, sum;
+
+    sum = x + y;  // Calculate the sum of x + y
+    System.out.println("Sum is: " + sum); // Print the sum
+  }
+} 
